733891ffeb0e2f801fcb811df93bbba3b4a42971
coderzzx <57378405+coderzzx25@users.noreply.github.com>
2023-01-27 17:03:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,2 @@
-/**
- * @Author: zzxcoder
- * @Date: 2022-09-15 21:44:01
- * @LastEditTime: 2022-10-11 14:22:24
- * @LastEditors: zzxcoder
- * @Description: zzxcoder
- * @FilePath: /lebang/src/service/config.js
- * @
- */
-export const BASE_URL = "http://localhost:8888"
+export const BASE_URL = ""
 export const TIEMOUT = 10000
-// http://249s4927a8.51vip.biz:29265
